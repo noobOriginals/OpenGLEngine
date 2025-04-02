086b0ace3343b7142b11533ba38b4b0ec2539bdd
noobOriginals <noobOriginals@xx.com>
2025-04-02 19:28:19 +0300

    Implemented shader uniforms

--- src/Header Files/api/shader.hpp
+++ src/Header Files/api/shader.hpp
@@ -27,25 +27,25 @@ public:
     void use();
     void stopUse();
     // Set uniforms 1
-    void setInt(int32 i, std::string name);
-    void setUnsignedInt(uint32 ui, std::string name);
-    void setFloat(float32 f, std::string name);
-    void setDouble(float64 d, std::string name);
+    void setInt(std::string name, int32 i);
+    void setUnsignedInt(std::string name, uint32 ui);
+    void setFloat(std::string name, float32 f);
+    void setDouble(std::string name, float64 d);
     // Set uniforms 2
-    void setInt2(int32 i0, int32 i1, std::string name);
-    void setUnsignedInt2(uint32 ui0, uint32 ui1, std::string name);
-    void setFloat2(float32 f0, float32 f1, std::string name);
-    void setDouble2(float64 d0, float64 d1, std::string name);
+    void setInt2(std::string name, int32 i0, int32 i1);
+    void setUnsignedInt2(std::string name, uint32 ui0, uint32 ui1);
+    void setFloat2(std::string name, float32 f0, float32 f1);
+    void setDouble2(std::string name, float64 d0, float64 d1);
     // Set uniforms 3
-    void setInt3(int32 i0, int32 i1, int32 i2, std::string name);
-    void setUnsignedInt3(uint32 ui0, uint32 ui1, uint32 ui2, std::string name);
-    void setFloat3(float32 f0, float32 f1, float32 f2, std::string name);
-    void setDouble3(float64 d0, float64 d1, float64 d2, std::string name);
+    void setInt3(std::string name, int32 i0, int32 i1, int32 i2);
+    void setUnsignedInt3(std::string name, uint32 ui0, uint32 ui1, uint32 ui2);
+    void setFloat3(std::string name, float32 f0, float32 f1, float32 f2);
+    void setDouble3(std::string name, float64 d0, float64 d1, float64 d2);
     // Set uniforms 4
-    void setInt4(int32 i0, int32 i1, int32 i2, int32 i3, std::string name);
-    void setUnsignedInt4(uint32 ui0, uint32 ui1, uint32 ui2, uint32 ui3, std::string name);
-    void setFloat4(float32 f0, float32 f1, float32 f2, float32 f3, std::string name);
-    void setDouble4(float64 d0, float64 d1, float64 d2, float64 d3, std::string name);
+    void setInt4(std::string name, int32 i0, int32 i1, int32 i2, int32 i3);
+    void setUnsignedInt4(std::string name, uint32 ui0, uint32 ui1, uint32 ui2, uint32 ui3);
+    void setFloat4(std::string name, float32 f0, float32 f1, float32 f2, float32 f3);
+    void setDouble4(std::string name, float64 d0, float64 d1, float64 d2, float64 d3);
 private:
     void compileShaders(const char* vsource, const char* fsource);
     uint32 address;
--- src/Source Files/api/shader.cpp
+++ src/Source Files/api/shader.cpp
@@ -4,7 +4,7 @@ using namespace api::shader;
 // Std includes
 #include <fstream>
 
-const char* def_vsource = 
+const char* def_vsource =
 "#version 330 core\n"
 "layout (location = 0) in vec3 pos;\n"
 "void main() {\n"
@@ -34,14 +34,14 @@ namespace shader {
 }
 Shader::Shader() {
     if (!gluIsGLFWInitialized()) throw std::runtime_error("No windows were created, so GLAD is probably not initialized.");
-    
+
     if (callouts) std::cout << "Shader: Creating shader nr. " << shaderCount << " with default sources.\n";
     compileShaders(def_vsource, def_fsource);
     shaderCount = shaderCount + 1;
 }
 Shader::Shader(std::string vsource, std::string fsource) {
     if (!gluIsGLFWInitialized()) throw std::runtime_error("No windows were created, so GLAD is probably not initialized.");
-    
+
     if (callouts) std::cout << "Shader: Creating shader nr. " << shaderCount << " with custom sources.\n";
     compileShaders(vsource.c_str(), fsource.c_str());
     shaderCount++;
@@ -58,6 +58,58 @@ void Shader::use() {
 }
 void Shader::stopUse() {
     glUseProgram(0);
+}
+// Set uniforms 1
+void Shader::setInt(std::string name, int32 i) {
+    glUniform1i(glGetUniformLocation(address, name.c_str()), i);
+}
+void Shader::setUnsignedInt(std::string name, uint32 ui) {
+    glUniform1ui(glGetUniformLocation(address, name.c_str()), ui);
+}
+void Shader::setFloat(std::string name, float32 f) {
+    glUniform1f(glGetUniformLocation(address, name.c_str()), f);
+}
+void Shader::setDouble(std::string name, float64 d) {
+    glUniform1d(glGetUniformLocation(address, name.c_str()), d);
+}
+// Set uniforms 2
+void Shader::setInt2(std::string name, int32 i0, int32 i1) {
+    glUniform2i(glGetUniformLocation(address, name.c_str()), i0, i1);
+}
+void Shader::setUnsignedInt2(std::string name, uint32 ui0, uint32 ui1) {
+    glUniform2ui(glGetUniformLocation(address, name.c_str()), ui0, ui1);
+}
+void Shader::setFloat2(std::string name, float32 f0, float32 f1) {
+    glUniform2f(glGetUniformLocation(address, name.c_str()), f0, f1);
+}
+void Shader::setDouble2(std::string name, float64 d0, float64 d1) {
+    glUniform2d(glGetUniformLocation(address, name.c_str()), d0, d1);
+}
+// Set uniforms 3
+void Shader::setInt3(std::string name, int32 i0, int32 i1, int32 i2) {
+    glUniform3i(glGetUniformLocation(address, name.c_str()), i0, i1, i2);
+}
+void Shader::setUnsignedInt3(std::string name, uint32 ui0, uint32 ui1, uint32 ui2) {
+    glUniform3ui(glGetUniformLocation(address, name.c_str()), ui0, ui1, ui2);
+}
+void Shader::setFloat3(std::string name, float32 f0, float32 f1, float32 f2) {
+    glUniform3f(glGetUniformLocation(address, name.c_str()), f0, f1, f2);
+}
+void Shader::setDouble3(std::string name, float64 d0, float64 d1, float64 d2) {
+    glUniform3d(glGetUniformLocation(address, name.c_str()), d0, d1, d2);
+}
+// Set uniforms 4
+void Shader::setInt4(std::string name, int32 i0, int32 i1, int32 i2, int32 i3) {
+    glUniform4i(glGetUniformLocation(address, name.c_str()), i0, i1, i2, i3);
+}
+void Shader::setUnsignedInt4(std::string name, uint32 ui0, uint32 ui1, uint32 ui2, uint32 ui3) {
+    glUniform4ui(glGetUniformLocation(address, name.c_str()), ui0, ui1, ui2, ui3);
+}
+void Shader::setFloat4(std::string name, float32 f0, float32 f1, float32 f2, float32 f3) {
+    glUniform4f(glGetUniformLocation(address, name.c_str()), f0, f1, f2, f3);
+}
+void Shader::setDouble4(std::string name, float64 d0, float64 d1, float64 d2, float64 d3) {
+    glUniform4d(glGetUniformLocation(address, name.c_str()), d0, d1, d2, d3);
 }
 
 // Private methods
@@ -78,7 +130,7 @@ void Shader::compileShaders(const char* vsource, const char* fsource) {
         std::cout << "Shader: Failed to compile vertex shader:\n" << infoLog << "\n";
     }
     if (callouts) std::cout << "Shader: Successfully compiled vertex shader\n";
-    
+
     // Compile fragment shader
     uint32 fshader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fshader, 1, &fsource, nullptr);
@@ -90,10 +142,10 @@ void Shader::compileShaders(const char* vsource, const char* fsource) {
         std::cout << "Shader: Failed to compile fragment shader:\n" << infoLog << "\n";
     }
     if (callouts) std::cout << "Shader: Successfully compiled fragment shader\n";
-    
+
     // Create program
     address = glCreateProgram();
-    
+
     glAttachShader(address, vshader);
     glAttachShader(address, fshader);
     glLinkProgram(address);
